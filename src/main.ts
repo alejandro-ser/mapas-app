f11ccbcf90ba369feb79c578c72f3cde9ed04eca
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-01-30 20:06:40 +0100

    Adding map with mapbox

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const Mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlam9zZWdpIiwiYSI6ImNrejFsZm14ajBoYW0yd3QyaXYwYzRkd3cifQ.VkAU8r8luxt69IcuK94KSw';
+
+if ( !navigator.geolocation) {
+  throw new Error('El navegador no soporta la geolocalización');
+}
+
 if (environment.production) {
   enableProdMode();
 }
