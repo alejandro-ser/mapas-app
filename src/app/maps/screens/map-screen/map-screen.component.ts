0f5cd1ea8e5965ef53d0b9ad7d6fcd1655e04091
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-01-30 16:58:26 +0100

    Initial structure setup

---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-screen',
+  templateUrl: './map-screen.component.html',
+  styleUrls: ['./map-screen.component.css']
+})
+export class MapScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
