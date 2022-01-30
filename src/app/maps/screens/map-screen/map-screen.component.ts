f14936f39412e1d7d3ae2d7a5fe0dad0bfdb8453
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-01-30 19:40:48 +0100

    Service for getting geolocation

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
   styleUrls: ['./map-screen.component.css']
 })
-export class MapScreenComponent implements OnInit {
+export class MapScreenComponent {
 
-  constructor() { }
+  constructor( private placesService: PlacesService) { }
 
-  ngOnInit(): void {
+  get isUserLocationReady() {
+    return this.placesService.isUserLocationReady;
   }
 
 }
