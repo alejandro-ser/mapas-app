1e877e53eb3ae8c8fd73e272fad8e7bc2b54c4db
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-02-01 17:06:17 +0100

    Search debounce timer

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+
+  private debounceTimer?: NodeJS.Timeout;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onQueryChanged( query: string = '' ) {
+
+    if ( this.debounceTimer ) clearTimeout( this.debounceTimer );
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar este query: ', query);
+    }, 400)
+
   }
 
 }
