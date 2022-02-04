0822e57da6207ebfdd4861209c912b3b6a47976b
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-02-04 19:39:22 +0100

    Add markers of finded places

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MapService } from '.';
 import { PlacesApiClient } from '../api';
 import { Feature, PlacesResponse } from '../interfaces/places';
 
@@ -17,7 +17,10 @@ export class PlacesService {
     return !!this.useLocation
   }
 
-  constructor( private placesApi: PlacesApiClient ) {
+  constructor(
+      private placesApi: PlacesApiClient,
+      private mapService: MapService
+    ) {
     this.getUserLocation();
   }
 
@@ -59,6 +62,8 @@ export class PlacesService {
         .subscribe( resp => {
           this.isLoadingPlaces = false;
           this.places = resp.features;
+
+          this.mapService.createMarkersFromPlaces( this.places );
         } );
   }
 }
